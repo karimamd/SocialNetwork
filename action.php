7441ea52841dead6a5240f5d59411e7e369a7d96
Kareem Ahmed Abdel-salam <karimamd95@yahoo.com>
2017-12-19 11:32:34 +0200

    kareem branch

--- action.php
+++ action.php
@@ -8,13 +8,15 @@ function SignUp() {
 	$query=$link->query("SELECT * FROM student WHERE email = '$_POST[email]'");
 if($query->num_rows>0){ echo "Already another account with the same email exists.
    Please log in. Redirecting after 5 seconds.";header('refresh: 5; url=index.php'); }
+
  else {
 
-        $query2=$link->query("INSERT INTO student ( firstName, lastName, email, pass )
-        VALUES ( '$_POST[fname]', '$_POST[lname]','$_POST[email]','$_POST[password]' );");
+        $query2=$link->query("INSERT INTO student ( FName, LName,Nick, Email, Pass )
+        VALUES ( '$_POST[fname]', '$_POST[lname]','$_POST[nname]','$_POST[email]','$_POST[password]' );");
 
         $_SESSION['user_email'] = $_POST['email'];header('Location: view.php');
       }
+
  }
 if(isset($_POST['submit']))
 {
