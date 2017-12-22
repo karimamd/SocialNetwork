3fdf89cafba70d002d24f3264eb5a8a2491dffb2
Kareem Ahmed Abdel-salam <karimamd95@yahoo.com>
2017-12-22 17:49:28 +0200

    reject button now works need to redirect

--- acceptFriendRequest.php
+++ acceptFriendRequest.php
@@ -1,21 +1,21 @@
 <?php
 session_start();
-$_SESSION['requesterID']=$_GET["friendRequest"];
+$requesterID=$_GET["friendRequest"];
 require 'SQLconnect.php';
 $conn = openConnection();
 //TODO remove request from database here or from showing in friendRequests page
 //TODO current problem is that I need to pass friend ID or friendRequest ID to
 //accept or refuse it
-$deleteSQL="DELETE from FriendRequest WHERE ProfileID_1=".$_SESSION['requesterID']."
+$deleteSQL="DELETE from FriendRequest WHERE ProfileID_1=".$requesterID."
  AND ProfileID_2=".$_SESSION['ProfileID'];
 $conn->query($deleteSQL);
  echo "friend request deleted";
 
   $insertSQL="INSERT INTO Friend(ProfileID_1,ProfileID_2)
-  VALUES (" . $_SESSION['requesterID'] . "," . $_SESSION['ProfileID'] . ")";
+  VALUES (" . $requesterID . "," . $_SESSION['ProfileID'] . ")";
   $result = $conn->query($insertSQL);
 
-    echo "user  of ID " . $_SESSION['requesterID'] .
+    echo "user  of ID " . $requesterID .
      "is now a friend with user with ID:".$_SESSION['ProfileID'];
 
 //need to redirect to friendRequests page
--- refuseFriendRequest.php
+++ refuseFriendRequest.php
@@ -1,22 +1,18 @@
 <?php
 session_start();
-$state=$_GET["friendRequest"];
+$requesterID=$_GET["friendRequest"];
 require 'SQLconnect.php';
 $conn = openConnection();
 //TODO remove request from database here or from showing in friendRequests page
 //TODO current problem is that I need to pass friend ID or friendRequest ID to
 //accept or refuse it
-$deleteSQL="DELETE from FriendRequest WHERE ProfileID_1=$_SESSION['requesterID']
- AND ProfileID_2=$_SESSION['ProfileID'] "
- $result = $conn->query($deleteSQL);
- if ($result->num_rows > 0 && $result->num_rows <3 ) {
-   echo "friend request deleted";
- }
- $row=$result->fetch_assoc();
- if ($state == "refuse") {
-  echo "user  of ID ". $row['ProfileID_2'] .
-   "refused to be friend with user with ID:".$row['ProfileID_1'] ;;
-}
+$deleteSQL="DELETE from FriendRequest WHERE ProfileID_1=".$requesterID."
+ AND ProfileID_2=".$_SESSION['ProfileID'];
+$conn->query($deleteSQL);
+ echo "friend request deleted";
+  echo "user  of ID ". $_SESSION['ProfileID'] .
+   "refused to be friend with user with ID:".$requesterID;
+
 
 //need to redirect to friendRequests page
 $conn->close();
